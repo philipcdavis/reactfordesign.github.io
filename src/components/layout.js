8fc8b592aabbd71eb7f890acb49d3ce2630da58b
Philip Davis <Phillysoul11@gmail.com>
2018-11-10 07:05:52 +0800

    Initial gatsby

---
+++ src/components/layout.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from 'gatsby'
+import styled, { injectGlobal } from "styled-components"
+
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Chivo:400,700');
+
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: Chivo, sans-serif;
+  }
+`
+
+const Container = styled.div`
+  margin: 3rem auto;
+  max-width: 600px;
+  width: 92%;
+`
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+
+        <Container>
+          {children}
+        </Container>
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
