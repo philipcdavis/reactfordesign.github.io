71d8d4ef2b8a0c8f0bfc03e2cc7ea9735ff9a018
Philip Davis <Phillysoul11@gmail.com>
2019-01-28 18:09:12 +0800

    Added meta tags

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,26 +15,38 @@ const Layout = ({ children }) => (
         }
       }
     `}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-          link={[
-              { rel: 'shortcut icon', type: 'image/png', href: FavIcon }
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
+    render={data => {
+      var image = "https://farm2.staticflickr.com/1849/43676135265_5d6be8f15a_o.png"
+      var description = "React is a powerful design tool. There's a lot you can do with it that would be difficult with a traditional design application. Prototyping with realistic data sets, creating complex interactions and shared component libraries are just the tip of the iceberg."
+      return (
+        <>
+          <Helmet
+            title={data.site.siteMetadata.title}
+            link={[
+                { rel: 'shortcut icon', type: 'image/png', href: FavIcon }
+            ]}
+          >
+            <html lang="en" />
+            <meta property="og:url" content="https://react.design" />
+            <meta property="og:type" content="website" />
+            <meta property="og:locale" content="en" />
+            <meta property="og:site_name" content="React for Design" />
+            <meta property="og:image" content={image} />
+            <meta property="og:image:width" content="512" />
+            <meta property="og:image:height" content="512" />
 
-        <div>
-          {children}
-        </div>
-      </>
-    )}
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@philipcdavis" />
+            <meta name="twitter:image" content={image} />
+            <meta name="twitter:description" content={description} />
+          </Helmet>
+
+          <div>
+            {children}
+          </div>
+        </>
+      )
+    }}
   />
 )
 
