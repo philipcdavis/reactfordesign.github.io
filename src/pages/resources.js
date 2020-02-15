6ee729eeac1d4ec736e84578a7ad6e4d62dbe412
Philip Davis <Phillysoul11@gmail.com>
2020-02-14 23:50:23 -0800

    Added filtering

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -5,11 +5,17 @@ import { GlobalStyle, color, fontSize } from '../Tile'
 import { Box, Flex, Text, Link } from 'rebass'
 import CodeAnimation from '../components/CodeAnimation'
 import PostFooter from '../templates/PostFooter'
-import Intro from '../components/Intro'
 import Logo from '../components/Logo'
 import { BtnRainbow } from '../components/Button'
 import { OutboundLink } from 'gatsby-plugin-google-analytics'
 import data from '../../static/data/resources'
+
+const tagColor = {
+  "getting-started": "green",
+  "library": "blue",
+  "article": "purple",
+  "video": "red",
+}
 
 export default function Free() {
   const [filter, setFilter] = useState();
@@ -20,38 +26,49 @@ export default function Free() {
       setFilter(label)
     }
   }
+
+
+  const filteredData = data.filter( function (resource) {
+    return filter ? resource.tags.includes(filter) : resource;
+  });
+
   return (
     <div>
       <GlobalStyle />
       <Layout>
 
-        <Box mt={5} ml={"60px"}>
-          <Tag active={filter === "getting-started" ? 1 : 0} onClick={() => toggleFilter("getting-started")} color="green">#getting-started</Tag>
-          <Tag active={filter === "library" ? 1 : 0} onClick={() => toggleFilter("library")} color="blue">#library</Tag>
-          <Tag active={filter === "article" ? 1 : 0} onClick={() => toggleFilter("article")} color="purple">#article</Tag>
-          <Tag active={filter === "video" ? 1 : 0} onClick={() => toggleFilter("video")} color="red">#video</Tag>
+        <Box width={["700px"]} mx="auto" mt={5}>
+          <Tag name="getting-started" filter={filter} onClick={() => toggleFilter("getting-started")} color="green">#getting-started</Tag>
+          <Tag name="library" filter={filter} onClick={() => toggleFilter("library")} color="blue">#library</Tag>
+          <Tag name="article" filter={filter} onClick={() => toggleFilter("article")} color="purple">#article</Tag>
+          <Tag name="video" filter={filter} onClick={() => toggleFilter("video")} color="red">#video</Tag>
         </Box>
-        <Box width={["600px"]} pt={5}>
+
+        <Box width={["700px"]} mx="auto" pt={5}>
+
           {
-            data.map(function(resource, i) {
+            filteredData.map(function(resource, i) {
               const title = resource.title;
               const description = resource.description;
               const url = resource.url;
               const tags = resource.tags;
-              console.log(tags)
 
               return (
-                <ResourceCard
-                  key={i}
-                  px={4}
-                  py={3}
-                  href="https://react.design"
-                >
-                  {tags.map((tag, i) => <Status tag={tag} key={i} /> )}
+                <Box key={i}>
+                  <ResourceCard
+                    key={i}
+                    py={3}
+                    href={url}
+                  >
+                    <StatusContainer>
+                      {tags.map((tag, i) => <Status id={i} key={i} kind={tag} /> )}
+                    </StatusContainer>
 
-                  <Text fontSize={3}>{title}</Text>
-                  <Text color={color.gray1} fontSize={1}>{getHostName(url)}</Text>
-                </ResourceCard>
+                    <Title fontSize={3} >{title}</Title>
+                    <Url display='inline-block' color={color.gray1} fontSize={2}>{ getHostName(url) }</Url>
+
+                  </ResourceCard>
+                </Box>
               )
           })}
         </Box>
@@ -61,12 +78,28 @@ export default function Free() {
   )
 }
 
+const StatusContainer = styled(Box)`
+  position: relative;
+`
 
 const Status = styled(Box)`
-  width: 5px;
-  height: 5px;
+  width: 12px;
+  height: 12px;
   border-radius: 10px;
-  background-color: red;
+  background-color: ${ props => color[tagColor[props.kind]]} ;
+  position: absolute;
+  left: ${props => (props.id * -8) - 20}px;
+  border: 2px solid ${color.black};
+  top: 5px;
+`
+
+const Url = styled(Text)`
+  display: inline-block;
+`
+
+const Title = styled(Text)`
+  display: inline-block;
+  margin-right: 7px;
 `
 
 const ResourceCard = styled(Link)`
@@ -75,10 +108,13 @@ const ResourceCard = styled(Link)`
   display: inline-block;
   border-radius: 12px;
   transition: 0.1s border-color ease-in;
-  width: 100%;
   &:hover {
-    background-color: rgba(0,0,0,0.4);
+    background-color: rgba(255,255,255,0.02);
   }
+  line-height: 1;
+  margin-bottom: 10px;
+  padding-left: 40px;
+  padding-right: 30px;
 `
 
 const Nav = styled(Flex)`
@@ -101,8 +137,8 @@ const Tag = styled(Link)`
   display: inline-block;
   font-weight: bold;
   font-size: ${fontSize[1]};
-  color: ${props => props.active ? color.black : color[props.color]};
-  background-color: ${props => props.active ? color[props.color] : "transparent"};
+  color: ${props => props.filter === props.name ? color.black : color[props.color]};
+  background-color: ${props => props.filter === props.name ? color[props.color] : "transparent"};
   padding: 3px 17px;
   margin-right: 10px;
   border-radius: 50px;
@@ -126,5 +162,3 @@ function getHostName(url) {
     }
   }
 }
-
-// https://spreadsheets.google.com/feeds/list/1Fxjkqg_Bc_76fvvuiErt6O3RUebzg4VrIVYtOz_cosE/od6/public/values?alt=json
