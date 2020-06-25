f60072270731498f9c2e9893c349848c86556192
Philip Davis <Phillysoul11@gmail.com>
2020-06-25 16:21:12 -0700

    Updated wording

--- components/Lessons.js
+++ components/Lessons.js
@@ -15,7 +15,7 @@ const Lessons = function(props) {
   return (
     <LessonContainer pt={[4,5]} mb={5}>
       <Box sx={{maxWidth: 700}}mt={4} >
-        <Heading color="white" mb={2} fontSize={[4,5]}>Lessons made for designers</Heading>
+        <Heading color="white" mb={2} fontSize={[4,5]}>React tailored to designers</Heading>
         <Text>There are plenty of great ways to learn React. This course is focused on the parts of React that are most useful for designers. I've worked hard to create lessons that are laser focused to this end. Below is the list of videos that are currently available and a those that are still in the works.</Text>
       </Box>
       <LessonCards />
