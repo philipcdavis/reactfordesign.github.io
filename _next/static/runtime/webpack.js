afc6138008ecc0f359315092420fbfad56f50b31
Philip Davis <philipcdavis@gmail.com>
2020-10-01 12:23:49 -0400

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3f4f60aa7a26e52e5731";
+/******/ 	var hotCurrentHash = "14f78c289dbee8021846";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
