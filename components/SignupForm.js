5b7ffde55add81efac66bdd8544f226880fe82d8
Philip Davis <philipcdavis@gmail.com>
2020-10-05 12:45:17 -0400

    Fixed sign up form responsive bug

--- components/SignupForm.js
+++ components/SignupForm.js
@@ -13,18 +13,18 @@ class SignupForm extends Component {
           <Flex id="mc_embed_signup_scroll" mx="auto">
             <Box flexGrow={1} className="mc-field-group">
             	<InputDark group type="email" placeholder="Email Address" name="EMAIL" className="required email" id="mce-EMAIL" />
-            </Box>
-          	<span id="mce-responses" className="clear">
-          		<div className="response" id="mce-error-response" style={{display:"none"}}></div>
-          		<div className="response" id="mce-success-response" style={{display:"none"}}></div>
-          	</span>
+              <span id="mce-responses" className="clear">
+                <div className="response" id="mce-error-response" style={{display:"none"}}></div>
+                <div className="response" id="mce-success-response" style={{display:"none"}}></div>
+              </span>
 
-            <div style={{position: "absolute", left: -5000}} aria-hidden="true">
-              <input type="text" name="b_4b8048aee994b838162f80e3a_21229251bb" tabIndex="-1" defaultValue="" />
-            </div>
-            <Box>
-              <BtnInput as="input" type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" />
+              <div style={{position: "absolute", left: -5000}} aria-hidden="true">
+                <input type="text" name="b_4b8048aee994b838162f80e3a_21229251bb" tabIndex="-1" defaultValue="" />
+              </div>
             </Box>
+            
+            <BtnInput as="input" type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" />
+            
           </Flex>
         </form>
       </div>
